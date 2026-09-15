ce27a184377e6075165b7987bb1ffef3020c0e8f
ms1701240-cell <ms1701240@gmail.com>
2026-09-15 15:08:27 +0300

    Initial commit

--- src/app/core/enviroment/url..ts
+++ src/app/core/enviroment/url..ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://6aa930fa2d442cb69d4986c6.mockapi.io'
 };
--- src/app/core/services/loanapi.ts
+++ src/app/core/services/loanapi.ts
@@ -1,31 +1,43 @@
-import { Injectable,inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../enviroment/url.';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs'; // ضفنا map هنا
 import { LoanItem, Employee, Treasury } from '../Models/loan.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class Loanapi {
   private http = inject(HttpClient);
   private apiUrl = environment.apiUrl;
+
   getLoans(): Observable<LoanItem[]> {
     return this.http.get<LoanItem[]>(`${this.apiUrl}/loans`);
   }
+
+  // بنستخرج القائمة من جوه الأوبجكت المدمج
   getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.apiUrl}/employees`);
+    return this.http.get<any[]>(`${this.apiUrl}/employees`).pipe(
+      map(res => res[0]?.employees || [])
+    );
   }
+
+  // نفس الكلام للخزائن، بنسحبها من نفس الـ endpoint
   getTreasuries(): Observable<Treasury[]> {
-    return this.http.get<Treasury[]>(`${this.apiUrl}/treasuries`);
+    return this.http.get<any[]>(`${this.apiUrl}/employees`).pipe(
+      map(res => res[0]?.treasuries || [])
+    );
   }
+
   addLoan(loan: LoanItem): Observable<LoanItem> {
     return this.http.post<LoanItem>(`${this.apiUrl}/loans`, loan);
   }
-  updateLoan(id: number, loan: LoanItem): Observable<LoanItem> {
+
+  updateLoan(id: number | string, loan: LoanItem): Observable<LoanItem> {
     return this.http.put<LoanItem>(`${this.apiUrl}/loans/${id}`, loan);
   }
-  deleteLoan(id: number): Observable<void> {
+
+  deleteLoan(id: number | string): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/loans/${id}`);
   }
- 
 }
